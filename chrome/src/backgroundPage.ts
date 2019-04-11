61cfa32464b93a80d26eeaae00bff47a60dd0371
Andrey Selemenev <andreyjkee@gmail.com>
2019-04-11 22:45:03 +0300

    open extension window on click to icon

--- chrome/src/backgroundPage.ts
+++ chrome/src/backgroundPage.ts
@@ -1,1 +1,16 @@
 console.log('background script loaded');
+
+var openPoddy = () => {
+    chrome.tabs.query({url: 'chrome-extension://*/index.html'}, (tabs) => {
+        if (tabs.length) {
+            chrome.tabs.update(tabs[0].id, {active: true});
+            chrome.windows.update(tabs[0].windowId, {focused: true});
+        } else {
+            window.open('/index.html');
+        }
+    });
+};
+
+chrome.browserAction.onClicked.addListener(() => {
+    openPoddy();
+});
--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,23 @@ import { AppModule } from './app/app.module';
 import { TAB_ID } from './app/tab-id.injector';
 import { environment } from './environments/environment';
 
-chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-  if (environment.production) {
-    enableProdMode();
-  }
 
-  const tab = [...tabs].pop();
-  const { id: tabId } = tab;
+if (environment.production) {
+  enableProdMode();
+}
+if (chrome.tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
 
-  // provides the current Tab ID so you can send messages to the content page
-  platformBrowserDynamic([{ provide: TAB_ID, useValue: tabId }])
-    .bootstrapModule(AppModule)
-    .catch(error => console.error(error));
-});
+
+    const tab = [...tabs].pop();
+    const { id: tabId } = tab;
+
+    // provides the current Tab ID so you can send messages to the content page
+    platformBrowserDynamic([{ provide: TAB_ID, useValue: tabId }])
+        .bootstrapModule(AppModule)
+        .catch(error => console.error(error));
+  });
+} else {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+      .catch(err => console.error(err));
+}
