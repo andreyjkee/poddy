81e302c3d6346f67f8b90ff98210f53f1cb902c3
Andrey Selemenev <andreyjkee@gmail.com>
2019-04-17 22:54:11 +0300

    add service to search podcast in itunes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared';
+import { ITunesService } from './services';
 
 @NgModule({
     declarations: [
@@ -11,10 +13,13 @@ import { SharedModule } from './shared';
     ],
     imports: [
         BrowserModule,
+        HttpClientModule,
         AppRoutingModule,
         SharedModule
     ],
-    providers: [],
+    providers: [
+        ITunesService
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/services/itunes.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+/**
+ * iTunes service api docs available here:
+ * https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
+ */
+@Injectable({
+    providedIn: 'root'
+})
+export class ITunesService {
+
+    baseUrl = 'https://itunes.apple.com';
+
+    constructor(protected httpClient: HttpClient) {
+    }
+
+    public search({term, limit = 20} : {term: string, limit: number}): Observable<any> {
+        const params = new HttpParams({
+            fromObject: {
+                media: 'podcast',
+                term: encodeURI(term),
+                limit: String(limit)
+            }
+        });
+        return this.httpClient.get(`${this.baseUrl}/search`, { params });
+    }
+
+}
